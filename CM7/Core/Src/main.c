8c408c2852d060fa7e87089197bdfc4510d0c8d0
jibonaronno <tebunia@gmail.com>
2023-12-17 11:02:35 +0900

    Updating synth

--- CM7/Core/Src/main.c
+++ CM7/Core/Src/main.c
@@ -300,7 +300,7 @@ Error_Handler();
 
     /* USER CODE BEGIN 3 */
 	  HAL_Delay(1000);
-	  HAL_UART_Transmit(&huart3, "ADC1 : \r\n", 9, -1);
+	  HAL_UART_Transmit(&huart3, "ADC1 : \r\n", 9, -1); // 4th param need -1
 	  HAL_UART_Transmit(&hlpuart1, "ADC0 : \r\n", 9, -1);
   }
   /* USER CODE END 3 */
